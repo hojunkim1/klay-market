4135818e6f5a6f8412c6f33db82c79fc0f3c6b4e
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-08-05 17:56:36 +0900

    add constants

---
+++ src/constants/constants.cypress.js
@@ -0,0 +1,5 @@
+export const ACCESS_KEY_ID = "0x00";
+export const SECRET_KEY_ID = "0x00";
+export const CHAIN_ID = "8217"; // Cypress Mainnet: 8217, Baobab Testnet: 1001
+
+export const COUNT_CONTRACT_ADDRESS = "0x00";
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export * from "./constants.baobab";
+
+export const PRIVATE_KEY = "0x00";
+export const ADDRESS = "0x00";
