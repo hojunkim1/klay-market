4ce8b99095d49b875a19133ffa9831b421831df4
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-08-10 18:03:02 +0900

    Delete src/constants directory

--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const PRIVATE_KEY = "0x00";
-export const ADDRESS = "0x00";
-
-export const ACCESS_KEY_ID = "0x00";
-export const SECRET_KEY_ID = "0x00";
-export const CHAIN_ID = "8217"; // Cypress Mainnet: 8217, Baobab Testnet: 1001
-
-export const COUNT_CONTRACT_ADDRESS = "0x00";
